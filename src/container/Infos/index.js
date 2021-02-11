3032b746a31b96f9b3997e01285aabbd7183b434
olowolowotimi <olowolowotimilehin@yahoo.com>
2021-02-11 04:51:01 -0800

    first commit

--- src/container/Infos/index.js
+++ src/container/Infos/index.js
@@ -3,45 +3,33 @@ import Cards from "../../components/Cards";
 import Spinner from "../../components/spinner";
 import infoStyle from "./style/Infos.module.css";
 
-
 const Infos = () => {
-        const [data, setData] = React.useState([]);
+  const [data, setData] = React.useState([]);
+  const [isloading, setisloading] = React.useState(true);
 
   React.useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((data) => {
         setData(data);
+        setisloading(false)
         console.log(data);
       });
   }, []);
 
-  return(
-      <div>
-          
-          
-            
-        {data?.map((users, i) => (
-            <Cards key={i}
-            username={users.username}
-            name={users.name}
-            email={users.email}
-            phone={users.phone}
-            company={users.company ? users.company.name : ""}
-            website={users.website}
-            address={users.address}
-            />
-            
-        ))}
-       
-           
-          </div>
-      
-    )
-}
-      
-  
-    
-
+  const userList = data?.map((users, i) => (
+    <Cards
+      key={i}
+      username={users.username}
+      name={users.name}
+      email={users.email}
+      phone={users.phone}
+      company={users.company ? users.company.name : ""}
+      website={users.website}
+      address={users.address}
+    />
+  ));
+  return isloading ? <Spinner /> : userList;
+};
 
 export default Infos;
